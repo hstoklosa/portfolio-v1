4bc41c112f95a15d4d95df22e29733fb29305da0
imexotic <hubert.stoklosa23@gmail.com>
2024-02-09 19:02:02 +0000

    init repo

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <h1>
+                HSTOKLOSA
+                <span>.DEV</span>
+            </h1>
+        </main>
+    );
+}
