5efbbaca2ba19d366eacee5dc223d3f1a7d3d451
hstoklosa <hubert.stoklosa23@gmail.com>
2025-01-26 04:59:08 +0000

    upload remaining code for the portfolio part

---
+++ src/site.config.ts
@@ -0,0 +1,70 @@
+import type { SiteConfig } from '@/types'
+import type { AstroExpressiveCodeOptions } from 'astro-expressive-code'
+
+export const siteConfig: SiteConfig = {
+	/** Used both as a meta property (src/components/BaseHead.astro L:31 + L:49)
+	 *  and the generated satori png (src/pages/og-image/[slug].png.ts)
+	 */
+	author: 'hstoklosa',
+
+	/** Meta property used to construct the meta title property
+	 *  found within src/components/BaseHead.astro L:11
+	 */
+	title: 'Hubert Stoklosa',
+
+	/* Meta property used as the default description meta property */
+	description: 'The first iteration of a personal portfolio.',
+
+	/* HTML language property (src/layouts/Base.astro L:18) */
+	lang: 'en-GB',
+
+	/* Meta property (src/components/BaseHead.astro L:42) */
+	ogLocale: 'en_GB',
+
+	/* Date.prototype.toLocaleDateString() parameters (src/utils/date.ts) */
+	date: {
+		locale: 'en-GB',
+		options: {
+			day: 'numeric',
+			month: 'short',
+			year: 'numeric'
+		}
+	}
+}
+
+export const menuLinks: Array<{ title: string; path: string }> = [
+	{ title: 'Home', path: '/' },
+	{ title: 'Blog', path: '/blog/' }
+]
+
+// https://expressive-code.com/reference/configuration/
+export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
+	/** One dark, one light theme
+	 *  => https://expressive-code.com/guides/themes/#available-themes
+	 */
+	themes: ['dracula', 'github-light'],
+	themeCssSelector(theme, { styleVariants }) {
+		/** If both themes are available, then generate theme CSS
+		 *  selectors compatible with cactus-theme dark mode switch.
+		 */
+		if (styleVariants.length >= 2) {
+			const baseTheme = styleVariants[0]?.theme
+			const altTheme = styleVariants.find((v) => v.theme.type !== baseTheme?.type)?.theme
+			if (theme === baseTheme || theme === altTheme) return `[data-theme='${theme.type}']`
+		}
+
+		// otherwise return the default selector
+		return `[data-theme="${theme.name}"]`
+	},
+	useThemedScrollbars: false,
+	styleOverrides: {
+		frames: { frameBoxShadowCssValue: 'none' },
+		uiLineHeight: 'inherit',
+		codeFontSize: '0.875rem',
+		codeLineHeight: '1.7142857rem',
+		borderRadius: '4px',
+		codePaddingInline: '1rem',
+		codeFontFamily:
+			'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;'
+	}
+}
