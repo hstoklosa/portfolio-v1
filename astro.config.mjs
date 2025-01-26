126f257c181ecd03720d675bb36fa35adeb33238
hstoklosa <hubert.stoklosa23@gmail.com>
2025-01-26 05:07:05 +0000

    minor changes to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,35 +12,35 @@ import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.me',
-	integrations: [
-		expressiveCode(expressiveCodeOptions),
-		tailwind({
-			applyBaseStyles: false
-		}),
-		sitemap(),
-		mdx()
-	],
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		rehypePlugins: [
-			[
-				rehypeExternalLinks,
-				{
-					target: '_blank',
-					rel: ['nofollow, noopener, noreferrer']
-				}
-			]
-		],
-		remarkRehype: {
-			footnoteLabelProperties: {
-				className: ['']
-			}
-		}
-	},
-	prefetch: true,
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: { enabled: true }
-	})
+    site: 'https://example.me',
+    integrations: [
+        expressiveCode(expressiveCodeOptions),
+        tailwind({
+            applyBaseStyles: false
+        }),
+        sitemap(),
+        mdx()
+    ],
+    markdown: {
+        remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+        rehypePlugins: [
+            [
+                rehypeExternalLinks,
+                {
+                    target: '_blank',
+                    rel: ['nofollow, noopener, noreferrer']
+                }
+            ]
+        ],
+        remarkRehype: {
+            footnoteLabelProperties: {
+                className: ['']
+            }
+        }
+    },
+    prefetch: true,
+    output: 'server',
+    adapter: vercel({
+        webAnalytics: { enabled: true }
+    })
 })
